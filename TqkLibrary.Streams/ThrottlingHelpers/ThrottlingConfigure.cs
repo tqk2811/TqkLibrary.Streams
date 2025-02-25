023a6862fad78dd0556ab9cdbe0de6f3e5f911b6
tqk2811 <tqk2811@gmail.com>
2025-02-25 16:11:53 +0700

    Add project files.

---
+++ TqkLibrary.Streams/ThrottlingHelpers/ThrottlingConfigure.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Streams.ThrottlingHelpers
+{
+    public class ThrottlingConfigure
+    {
+        const int min_read = 1;
+
+        /// <summary>
+        /// delay in miliseconds
+        /// </summary>
+        public int DelayWriteStep { get; set; } = 10;
+
+        /// <summary>
+        /// Balanced for multi streams read/write. Maximum result for per step calc <see cref="CalcRead"/> and <see cref="CalcWrite"/>"/>
+        /// </summary>
+        public int Balanced { get; set; } = int.MaxValue;
+
+        /// <summary>
+        /// less or equal zero mean no limit
+        /// </summary>
+        public int ReadBytesPerTime { get; set; } = 0;
+
+        /// <summary>
+        /// less or equal zero mean no limit
+        /// </summary>
+        public int WriteBytesPerTime { get; set; } = 0;
+
+        /// <summary>
+        /// less or equal zero mean no limit
+        /// </summary>
+        public TimeSpan Time { get; set; } = TimeSpan.FromSeconds(1);
+
+
+        readonly object _lock_read = new();
+        DateTime _lastTimeRead = DateTime.MinValue;
+        int _readBytes = 0;
+        public int CalcRead(int count)
+        {
+            if (ReadBytesPerTime <= 0 || Time <= TimeSpan.Zero || count == 0) return count;
+            lock (_lock_read)
+            {
+                DateTime now = DateTime.UtcNow;
+                if (now > _lastTimeRead.Add(Time))
+                {
+                    _lastTimeRead = now;
+                    _readBytes = 0;
+                }
+
+                count = Math.Max(min_read, Math.Min(ReadBytesPerTime - _readBytes, Balanced <= 0 ? int.MaxValue : Balanced));
+                _readBytes += count;
+
+                return count;
+            }
+        }
+        public void UpdateRealRead(int count)
+        {
+            if(count > 0)
+            {
+                lock (_lock_read)
+                {
+                    _readBytes -= count;
+                }
+            }
+        }
+
+
+
+
+        readonly object _lock_write = new();
+        DateTime _lastTimeWrite = DateTime.MinValue;
+        int _writeBytes = 0;
+        public int CalcWrite(int count)
+        {
+            if (WriteBytesPerTime <= 0 || Time <= TimeSpan.Zero || count == 0) return count;
+            lock (_lock_write)
+            {
+                DateTime now = DateTime.UtcNow;
+                if (now > _lastTimeWrite.Add(Time))
+                {
+                    _lastTimeWrite = now;
+                    _writeBytes = 0;
+                }
+
+                count = Math.Max(0, Math.Min(WriteBytesPerTime - _writeBytes, Balanced <= 0 ? int.MaxValue : Balanced));
+                _writeBytes += count;
+
+                return count;
+            }
+        }
+    }
+}
---
+++ TqkLibrary.Streams/ThrottlingStream.cs
@@ -0,0 +1,63 @@
+﻿using TqkLibrary.Streams.ThrottlingHelpers;
+
+namespace TqkLibrary.Streams
+{
+    public class ThrottlingStream : BaseInheritStream
+    {
+        public ThrottlingConfigure Configure { get; } = new();
+
+        public ThrottlingStream(Stream baseStream, bool disposeBaseStream = true) : base(baseStream, disposeBaseStream)
+        {
+        }
+        public ThrottlingStream(ThrottlingConfigure configure, Stream baseStream, bool disposeBaseStream = true) : base(baseStream, disposeBaseStream)
+        {
+            this.Configure = configure ?? throw new ArgumentNullException(nameof(configure));
+        }
+
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
+            => throw new NotSupportedException();
+        public override int EndRead(IAsyncResult asyncResult)
+            => throw new NotSupportedException();
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
+            => throw new NotSupportedException();
+        public override void EndWrite(IAsyncResult asyncResult)
+            => throw new NotSupportedException();
+
+
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int calcCount = Configure.CalcRead(count);
+            int bytes_read = _baseStream.Read(buffer, offset, calcCount);
+            Configure.UpdateRealRead(calcCount - bytes_read);
+            return bytes_read;
+        }
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken = default)
+        {
+            int calcCount = Configure.CalcRead(count);
+            int bytes_read = await base.ReadAsync(buffer, offset, calcCount, cancellationToken);
+            Configure.UpdateRealRead(calcCount - bytes_read);
+            return bytes_read;
+        }
+
+
+
+        public override void Write(byte[] buffer, int offset, int count)
+            => throw new NotSupportedException();//mustbe delay, can't add
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken = default)
+        {
+            while (count > 0)
+            {
+                int stepCount = Configure.CalcWrite(count);
+                if (stepCount > 0)
+                {
+                    await _baseStream.WriteAsync(buffer, offset, stepCount, cancellationToken);
+
+                    offset += stepCount;
+                    count -= stepCount;
+                }
+                await Task.Delay(Math.Max(Configure.DelayWriteStep, 0), cancellationToken);
+            }
+        }
+    }
+}
