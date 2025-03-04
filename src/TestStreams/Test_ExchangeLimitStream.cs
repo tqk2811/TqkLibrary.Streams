80525da447410373405a1f4654d8e4aeeedef411
tqk2811 <tqk2811@gmail.com>
2025-03-04 16:52:48 +0700

    add ExchangeLimitStream

---
+++ src/TestStreams/Test_ExchangeLimitStream.cs
@@ -0,0 +1,36 @@
+﻿using TqkLibrary.Streams;
+
+namespace TestStreams
+{
+    [TestClass]
+    public sealed class Test_ExchangeLimitStream
+    {
+        [TestMethod]
+        public async Task Test()
+        {
+            using MemoryStream memoryStream = new();
+            ExchangeLimitStream exchangeLimitStream = new(memoryStream);
+            exchangeLimitStream.MaxBytesRead = 100;
+            exchangeLimitStream.MaxBytesWrite = 100;
+
+
+            byte[] buffer = new byte[Random.Shared.Next(200, 400)];
+            Random.Shared.NextBytes(buffer);
+            byte[] buffer2 = new byte[buffer.Length];
+
+
+            await exchangeLimitStream.WriteAsync(buffer);
+            Assert.AreEqual(memoryStream.Length, exchangeLimitStream.MaxBytesWrite);
+
+
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            await memoryStream.WriteAsync(buffer);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            using MemoryStream memoryStream2 = new();
+            await exchangeLimitStream.CopyToAsync(memoryStream2);
+            Assert.AreEqual(memoryStream2.Length, exchangeLimitStream.MaxBytesRead);
+        }
+    }
+}
---
+++ src/TqkLibrary.Streams/ExchangeLimitStream.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TqkLibrary.Streams
+{
+    public class ExchangeLimitStream : BaseInheritStream
+    {
+        /// <summary>
+        /// 0 mean unlimit
+        /// </summary>
+        public uint MaxBytesRead { get; set; } = uint.MaxValue;
+        /// <summary>
+        /// 0 mean unlimit
+        /// </summary>
+        public uint MaxBytesWrite { get; set; } = uint.MaxValue;
+
+        public ExchangeLimitStream(Stream baseStream, bool disposeBaseStream = true) : base(baseStream, disposeBaseStream)
+        {
+        }
+
+        uint _bytesReadedCount = 0;
+        readonly object _lockRead = new object();
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (MaxBytesRead != 0 && count > 0)
+            {
+                lock (_lockRead)
+                {
+                    uint diff = MaxBytesRead - _bytesReadedCount;
+                    count = (int)Math.Min(diff, (uint)count);
+                    if (count == 0) return count;
+                }
+            }
+            int byte_read = base.Read(buffer, offset, count);
+            if(byte_read > 0)
+            {
+                lock (_lockRead)
+                {
+                    _bytesReadedCount += (uint)byte_read;
+                }
+            }
+            return byte_read;
+        }
+
+
+        uint _bytesWritedCount = 0;
+        readonly object _lockWrite = new object();
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (MaxBytesWrite != 0 && count > 0)
+            {
+                lock (_lockWrite)
+                {
+                    uint diff = MaxBytesWrite - _bytesWritedCount;
+                    count = (int)Math.Min(diff, (uint)count);
+                    if (count == 0) return;
+                    _bytesWritedCount += (uint)count;
+                }
+            }
+            base.Write(buffer, offset, count);
+        }
+
+    }
+}
